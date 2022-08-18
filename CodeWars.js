f224d0a59c639cc9c5bcd8e2f81bb85f4688df34
Alexandre Santos <Alexandre_MFS@outlook.com>
2022-08-18 03:05:20 -0300

    'CodewarsTS'

--- CodeWars.js
+++ CodeWars.js
@@ -81,11 +81,28 @@ You can assume, for the purpose of this kata, that the supplied array will not b
 
 class SmallestIntegerFinder {
     findSmallestInt(args) {
-      return Math.max(...args)
+        return Math.max(...args)
     }
-  }
+}
+
+
+
+/* 7 kyu Shortest Word
+
+
+Simple, given a string of words, return the length of the shortest word(s).
+
+String will never be empty and you do not need to account for different data types.
+*/
+
+function findShort(s) {
+    return Math.min(...s.split(' ').map( x => x.length))
+   
+}
 
 
 
 
-  
+
+
+ 
