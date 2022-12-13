df7285b2e601a3f714f308e105d3e98a92ce3e8d
Alexandre Santos <Alexandre_MFS@outlook.com>
2022-12-13 09:56:09 -0300

    'Codewars'

--- CodeWars.ts
+++ CodeWars.ts
@@ -132,4 +132,25 @@ function breakChocolate(n:number, m:number):number {
 
     function squareDigits(num:number)    {
         return  +(num.toString().split('').map((x) => +x * +x).join(''))
+}
+
+
+/* 8 Kyu Quarter of the year
+
+Given a month as an integer from 1 to 12, return to which quarter of the year it belongs as an integer number.
+
+For example: month 2 (February), is part of the first quarter; month 6 (June), is part of the second quarter; and month 11 (November), is part of the fourth quarter.
+
+*/
+
+export function quarterOf(month: number): number {
+    if(month <=3){
+      return 1
+    }else if(month <= 6){
+      return 2
+    }else if (month <= 9){
+      return 3
+    }else{
+      return 4
     }
+   }
