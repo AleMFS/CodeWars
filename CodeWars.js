958626d497e51d71d02124f6c888c87c4c48219f
Alexandre Santos <Alexandre_MFS@outlook.com>
2022-12-21 21:39:16 -0300

    codewars js

--- CodeWars.js
+++ CodeWars.js
@@ -1,3 +1,16 @@
+/* 7 Kyu  -  Square Every Digit
+
+Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
+
+For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1.
+
+Note: The function accepts an integer and returns an integer */
+
+function squareDigits(num) {
+    return  Number(num.toString().split('').map((x) => +x * +x).join(''))
+}
+
+
 /* 8 kyu Cat years, Dog years
 
 I have a cat and a dog.
@@ -41,19 +54,9 @@ var humanYearsCatYearsDogYears = function(humanYears) {
   
 
 
-/* 7 Kyu  -  Square Every Digit
 
-Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
 
-For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1.
 
-Note: The function accepts an integer and returns an integer */
-
-function squareDigits(num) {
-    return  Number(num.toString().split('').map((x) => +x * +x).join(''))
-}
-
-console.log(squareDigits(122))
 
 
 
