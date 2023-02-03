9ea314abc34b799214e29bf5f009d62e4d950efe
Alexandre Santos <Alexandre_MFS@outlook.com>
2023-02-02 23:11:50 -0300

    02/02

--- CodeWars.ts
+++ CodeWars.ts
@@ -2,6 +2,7 @@
 
 /* Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
 
+
 For example (Input -> Output): */
 
 export function summation(num: number) {
